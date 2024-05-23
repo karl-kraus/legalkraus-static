7ea03368df78c855cf1b3c2c670fe3059d791799
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2024-05-23 14:04:27 +0000

    refactor: remove font related features for know

--- html/js/run.js
+++ html/js/run.js
@@ -125,28 +125,6 @@ var editor = new LoadEditor({
         slider_class: "i-slider round",
       },
     },
-    ff: {
-      name: "Change font family",
-      variants: [
-        {
-          opt: "ff",
-          title: "Font Family",
-          urlparam: "ff",
-          chg_citation: "citation-url",
-          fonts: {
-            default: "default",
-            font1: "Times-New-Roman",
-            font2: "Courier-New",
-            font3: "Arial-serif",
-          },
-          paragraph: ".yes-index",
-          p_class: "",
-          css_class: "",
-        },
-      ],
-      active_class: "active",
-      html_class: "form-select",
-    },
     fs: {
       name: "Create full size mode",
       variants: [
@@ -163,29 +141,6 @@ var editor = new LoadEditor({
       render_class: "nav-link btn btn-round",
       render_svg:
         "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'><path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg>",
-    },
-    fos: {
-      name: "Change font size",
-      variants: [
-        {
-          opt: "fs",
-          title: "Font Size",
-          urlparam: "fs",
-          chg_citation: "citation-url",
-          sizes: {
-            default: "default",
-            font_size_14: "14",
-            font_size_18: "18",
-            font_size_22: "22",
-            font_size_26: "26",
-          },
-          paragraph: ".yes-index",
-          p_class: "",
-          css_class: "font-size-",
-        },
-      ],
-      active_class: "active",
-      html_class: "form-select",
     },
     is: {
       name: "Facsimiles On/Off",
